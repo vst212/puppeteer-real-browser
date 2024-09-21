026f41d3a74f3e8848ba53ccc20abc5937bbf91b
vst212 <47769540+vst212@users.noreply.github.com>
2024-09-21 13:31:30 +0800

    Update test1.js

--- test/cjs/test1.js
+++ test/cjs/test1.js
@@ -1,1 +1,44 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const { connect } = require('../../lib/cjs/index.js');
 
+const url = "https://stake.com";
+
+const realBrowserOption = {
+    args: ["--start-maximized"],
+    turnstile: true,
+    headless: false,
+    // disableXvfb: true,
+    customConfig: {},
+    connectOption: {
+        defaultViewport: null
+    },
+    plugins: []
+}
+
+test('Cloudflare Turnstile', async () => {
+    const { page, browser } = await connect(realBrowserOption)
+    await page.goto(url);
+    await page.waitForSelector('[data-action="demo_action"]')
+    let token = null
+    let verify = null
+    let startDate = Date.now()
+    while (!verify && (Date.now() - startDate) < 30000) {
+        verify = await page.evaluate(() => { return document.querySelector('.link_row') ? true : null }).catch(() => null)
+        await new Promise(r => setTimeout(r, 1000));
+    }
+    while (!token && (Date.now() - startDate) < 30000) {
+        token = await page.evaluate(() => {
+            try {
+                let item = document.querySelector('[name="cf-turnstile-response"]').value
+                return item && item.length > 20 ? item : null
+            } catch (e) {
+                return null
+            }
+        })
+        await new Promise(r => setTimeout(r, 1000));
+    }
+    await browser.close()
+    // if (token !== null) console.log('Cloudflare Turnstile Token: ' + token);
+    assert.strictEqual(token !== null, true, "Cloudflare turnstile test failed!")
+})
